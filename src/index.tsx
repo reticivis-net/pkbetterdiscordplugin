af28c0dc65a31b812ddffbd8da431732555b5532
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-03-03 14:34:31 -0600

    fixes fixes fixes baby!

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,31 @@
 /// <reference types="zerespluginlibrarytypings"/>
 
-module.exports = (BasePlugin, Library: typeof ZLibrary) => {
+module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
 
-    const {Logger} = Library;
+    const {Logger, Patcher, WebpackModules} = Library;
 
-    return class PKBD extends BasePlugin {
+    return class PKBD extends Plugin {
         constructor() {
             super();
         }
 
         onStart() {
             Logger.log("Started");
+            debugger
+            const BotTag = WebpackModules.getByProps("BotTagTypes");
+            const BotTagTypes = BotTag.default.Types || BotTag.BotTagTypes;
+            // const MessagePack = WebpackModules.getByProps("BaseMessageHeader")
+            const MessagePack = BdApi.findModule(m => m?.default?.toString().indexOf("showTimestampOnHover") > -1);
+            Logger.log(BotTag, BotTagTypes, MessagePack, Patcher)
+            Patcher.instead(BotTag, "default", ((args) => {
+                console.log(args)
+                return "BALLSBALLSBALLS"
+            }))
         }
 
         onStop() {
             Logger.log("Stopped");
+            Patcher.unpatchAll();
         }
     }
 }
