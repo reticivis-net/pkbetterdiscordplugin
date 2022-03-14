6b124d1bb7021b095cd8488ec43f8c412c25dbaf
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-03-13 23:20:05 -0500

    epic reply!!!

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,17 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
     const markupClasses = WebpackModules.getByProps("markup");
     const clamped = WebpackModules.getByProps("clamped").clamped;
     const ProfileModals = WebpackModules.getByProps("openUserProfileModal");
+    const MessageStore = WebpackModules.getByProps("hasCurrentUserSentMessage", "getMessage");
+    const MemberStore = WebpackModules.getByProps("getMember");
+    const UserStore = WebpackModules.getAllByProps("getUser", "getUsers");
+    const MessagePack = WebpackModules.getByProps("BaseMessageHeader");
+    const RepliedMessage = WebpackModules.getModule(m => m.default.displayName === "RepliedMessage");
+    const replyclasses = WebpackModules.getByProps("replyAvatar");
+    const bottagclasses = WebpackModules.getByProps("botTagRegular");
+    const embedclasses = WebpackModules.getByProps("embedFull");
+    const MessageInterface = WebpackModules.getByProps("jumpToMessage");
+    window["pkjumptomessage"] = MessageInterface.jumpToMessage;
+
     const sleep = (ms: number) => {
         return new Promise((resolve) => {
             setTimeout(resolve, ms);
@@ -124,10 +135,36 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
         }
 
 
+        PKReplyHTML(messageid: string | number, author: string, authorcolor: string, replycontent: string, bottag: string, authoravatar: string, replyid: string | number, channelid: string | number) {
+            // TODO: try to use the native react reply generator so other plugins can use it
+
+            return `<div id="message-reply-context-${messageid}" class="${replyclasses.repliedMessage}"
+                        aria-label="replying to ${author}">
+                <img alt=""
+                     src="${authoravatar}"
+                     class="${replyclasses.replyAvatar} ${replyclasses.clickable}"/>
+                ${bottag ? `<span class="${replyclasses.botTagCompact} ${bottagclasses.botTagRegular} ${bottagclasses.rem}">
+                    <span class="${bottagclasses.botText}">${bottag}</span>
+                </span>` : ""}
+                <span class="${replyclasses.username} ${replyclasses.clickable}"
+                      role="button" tabIndex="0"
+                      style="color:${authorcolor}">${author}</span>
+                <div class="${replyclasses.repliedTextPreview} ${replyclasses.clickable}" role="button" onclick="window['pkjumptomessage']({channelId: '${channelid}',flash: true,messageId: '${replyid}',returnMessageId: '${messageid}'})"
+                     tabIndex="0"><div id="message-content-${replyid}" class="${replyclasses.repliedTextContent} ${markupClasses.markup} ${replyclasses.messageContent}">${replycontent.replace(/\n+/, " ")}</div></div>
+            </div>`
+        }
+
         PKPopout(membername: string, membercolor: string, memberavatar: string, systemname: string, memberdescription: string, accountid: string | number, guildid: string | number) {
 
+            function handleevent(t) {
+                // might be good
+                t.preventDefault();
+                t.stopPropagation();
+            }
+
             // opens acc modal dynamically, praise allah its so easy
-            function openmodal() {
+            function openmodal(t) {
+                handleevent(t)
                 ProfileModals.openUserProfileModal(
                     {
                         userId: accountid.toString(),
@@ -135,7 +172,20 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
                         analyticsLocation: {
                             "section": "Profile Popout"
                         }
-                    })
+                    });
+                // close modal, just a nice thing not needed
+                let currentelem = t.currentTarget.__reactFiber$
+                let func;
+                // find the function to close it waaaayyyy up the react tree
+                for (let i = 0; i < 30; i++) {
+                    if ((func = Utilities.getNestedProp(currentelem, "memoizedProps.onRequestClose"))) {
+                        func();
+                        break;
+                    } else {
+                        // return = parent
+                        currentelem = currentelem.return;
+                    }
+                }
             }
 
             // take PK data and make nice looking popout
@@ -210,15 +260,24 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
 
         livecache = {}
 
+        cdnbase = 'https://cdn.discordapp.com'
+
+        guildAvatarToUrl(asset: string, guild_id: string | number, member_id: string | number) {
+            // https://github.com/Rapptz/discord.py/blob/603681940fedf9f5640217f369352557e104d736/discord/asset.py#L182-L191
+            return `${this.cdnbase}/guilds/${guild_id}/users/${member_id}/avatars/${asset}.${asset.startsWith("a_") ? "gif" : "png"}`
+        }
+
+        avatarToUrl(asset: string, member_id: string | number) {
+            // https://github.com/Rapptz/discord.py/blob/603681940fedf9f5640217f369352557e104d736/discord/asset.py#L182-L191
+            return `${this.cdnbase}/avatars/${member_id}/${asset}.${asset.startsWith("a_") ? "gif" : "png"}`
+        }
+
         onStart() {
             // start the bucket, the check should stop the thing from running twice
             if (!this.plugin_active) {
                 this.plugin_active = true
                 this.bucketdrip()
             }
-            const MessagePack = WebpackModules.getByProps("BaseMessageHeader");
-
-            const RepliedMessage = WebpackModules.getModule(m => m.default.displayName === "RepliedMessage");
             Patcher.after(MessagePack, "default", (thisObject, args, returnValue) => {
                 let pkmemberdata = {};
                 const auth = returnValue.props.message.author;
@@ -245,7 +304,6 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
                     this.pkmessagedata(returnValue.props.message.id).then(r => {
                         // make sure no unneeded edits are made
                         if (r === undefined) return
-                        // add member to cache
 
                         // fetch member info for popup
                         this.pkmemberdata(r.member.uuid).then(m => {
@@ -260,6 +318,34 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
                         let header = document.getElementById(`message-username-${returnValue.props.message.id}`);
                         if (!header) return
                         if (header.hasAttribute("PKBDedited")) return
+
+                        const replyregex = /\*\*\[Reply to:]\(https:\/\/(ptb.|canary.)?discord(app)?.com\/channels\/(?<guild>\d+|@me)\/(?<channel>\d+)\/(?<message>\d+)\/?\)\*\* ?(?<mcontent>.*)/
+                        const embeddesc: string = Utilities.getNestedProp(returnValue, "props.message.embeds.0.rawDescription")
+                        if (embeddesc && replyregex.test(embeddesc)) {
+                            const m = embeddesc.match(replyregex)
+                            const ref = MessageStore.getMessage(m.groups["channel"], m.groups["message"])
+                            if (ref) {
+                                // make an empty object the default value so getting props of it doesn't fail
+                                const refmember = MemberStore.getMember(ref.guild.id, ref.author.id) || {};
+                                // remove embed
+                                // TODO: naive, rreally should be smarter bout this and make sre im actually removing the reply embed
+                                const replyembed = document.querySelector(`#message-accessories-${returnValue.props.message.id} > article`)
+                                if (replyembed) {
+                                    replyembed.remove();
+                                    const chatmessages = document.getElementById(`chat-messages-${returnValue.props.message.id}`);
+                                    chatmessages.firstElementChild.insertAdjacentHTML("afterbegin",
+                                        this.PKReplyHTML(returnValue.props.message.id, refmember.nick || ref.author.username,
+                                            refmember.colorString || "#fff", m.groups["mcontent"], ref.bot ? "BOT" : "",
+                                            refmember.avatar ? this.guildAvatarToUrl(refmember.avatar, ref.guild.id, ref.author.id) : this.avatarToUrl(ref.author.avatar, ref.author.id),
+                                            m.groups["message"], m.groups["channel"]))
+                                    // doesn't appear to do anything but better safe than sorry
+                                    chatmessages.classList.add(replyclasses.hasReply)
+                                }
+
+
+                            }
+                        }
+
                         let usernameelem = (header.children[0] as HTMLElement);
                         let servernick;
                         // server display names aren't sent for some god forsaken reason so we do a wee bit of parsing
@@ -274,7 +360,7 @@ module.exports = (Plugin: typeof BasePlugin, Library: typeof PluginLibrary) => {
                             servernick = usernameelem.innerText;
                         }
                         // edit username appearance
-                        usernameelem.innerHTML = `${servernick} <span style="color: #${r.system.color || 'fff'}">${r.system.tag || ""}</span>`;
+                        usernameelem.innerHTML = `${servernick.trim()}${r.system.tag ? " " : ""}<span style="color: #${r.system.color || 'fff'}">${(r.system.tag || "").trim()}</span>`;
                         // underline isnt colored if i dont do it like this sadly
                         usernameelem.style.color = "#" + (r.member.color || 'fff');
                         (header.children[1] as HTMLElement).firstElementChild.innerHTML = "PK";
